0ff7f66f2fccc92649cde4053ecb73c68a60cf32
lazzych <botbotlvic14@gmail.com>
2024-05-09 18:31:12 +0300

    restored

---
+++ main.py
@@ -0,0 +1,40 @@
+import pyautogui
+import os
+import subprocess
+import webbrowser
+
+pyautogui.alert('hello, user <3')
+while True:
+    ret = 1
+    commandline = str(input('~'))  ##ввод комманды
+    if commandline == "quit":
+        break
+    if commandline == "listdir":
+        print(os.listdir())
+        ret = 0
+    ##ahk
+    if commandline == "ahk":
+        os.chdir('ahk')
+        arg = str(input('~ahk/'))
+        ret = 0
+        if arg == "ahk.install":
+            print("~error: can't create process, will be fixed soon")
+            ret = 0
+        if arg == "return":
+            os.chdir('')
+            ret = 0
+        if arg == "cs.nulls":
+            pyautogui.alert('open console and press F3')
+            webbrowser.open('steam://rungameid/730', new=0, autoraise=False)
+            subprocess.run(['nullexec V1.exe'])
+            os.chdir('')
+            ret = 0
+
+        if ret == 1:
+            print('~', arg, ' ', 'is not found, returning')
+            os.chdir('..')
+
+
+
+    if ret==1:
+        print('~',commandline,' ', 'is not found')
