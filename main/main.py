95251c4e43bae7f29c055a27251ecd642d2ef2ac
lazzych <botbotlvic14@gmail.com>
2024-05-09 19:58:13 +0300

    recode((((

---
+++ main/main.py
@@ -0,0 +1,14 @@
+import pyautogui
+import os
+import subprocess
+import webbrowser
+
+pyautogui.alert('hello, user <3')
+ret=int(1)
+while True:
+    
+
+    if ret==int(1):
+        print("~err: not found")
+        ret = int(1)
+
