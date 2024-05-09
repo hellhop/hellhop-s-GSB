11970aadf9e389b678f9689d039c078c30ef003d
lazzych <botbotlvic14@gmail.com>
2024-05-09 12:35:21 +0300

    added "quit" command

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ pyautogui.alert('hello, user <3')
 while True:
     ret = 0
     commandline = str(input('~'))  ##ввод комманды
-    if commandline == 'testalert':
-        print(commandline)
-        pyautogui.alert('~ test alert')
+    if commandline=="gsb.list":
+        print('list is not printable now')
         ret = 1
 
-    if commandline == "install autohotkey":
-        subprocess.run(["AutoHotkey_1.1.37.02_setup.exe"])
+    if commandline == 'testalert':
+        pyautogui.alert('~test alert')
+        ret = 1
+
+    if commandline == "sgb.install autohotkey":
+        print('~AHK installer is not working, we will fix it soon')
+        ##subprocess.run(["AutoHotkey_1.1.37.02_setup.exe"])
         ret = 1
 
     if commandline == "quit":
         break
 
     if ret==0:
-        print('~', commandline, ' ', 'is not found')
+        print('~',commandline,' ', 'is not found')
