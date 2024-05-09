88d1f613783b193bc71a1d8b1d30a117c098ef1d
lazzych <botbotlvic14@gmail.com>
2024-05-09 15:46:16 +0300

    ahk

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import pyautogui
-import subprocess
-#import os
+import os
 
 pyautogui.alert('hello, user <3')
 while True:
-    ret = 0
+    ret = 1
     commandline = str(input('~'))  ##ввод комманды
-    if commandline=="gsb.list":
-        print('list is not printable now')
-        ret = 1
-
-    if commandline == 'testalert':
-        pyautogui.alert('~test alert')
-        ret = 1
-
-    if commandline == "sgb.install autohotkey":
-        print('~AHK installer is not working, we will fix it soon')
-        ##subprocess.run(["AutoHotkey_1.1.37.02_setup.exe"])
-        ret = 1
-
     if commandline == "quit":
         break
+    if commandline == "listdir":
+        print(os.listdir())
+        ret = 0
+    ##ahk
+    if commandline == "ahk":
+        os.chdir('ahk')
+        arg = str(input('~ahk/'))
+        ret = 0
+        if arg == "ahkinstall":
+            print("~error: can't create process, will be fixed soon")
+            ret = 0
+        if arg == "return":
+            os.chdir('..')
+            ret = 0
+        if arg == "cs.nulls":
+            #ahk launch
+            pyautogui.alert('run cs, open console and press F3')
+            os.chdir('..')
+            ret = 0
 
-    if ret==0:
+        if ret == 1:
+            print('~', arg, ' ', 'is not found, returning')
+            os.chdir('..')
+
+
+
+    if ret==1:
         print('~',commandline,' ', 'is not found')
