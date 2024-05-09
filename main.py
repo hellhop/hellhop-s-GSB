a7f783343145bc019a8ac0b084ac137c38457928
lazzych <botbotlvic14@gmail.com>
2024-05-09 11:42:03 +0300

    tested pyautogui and added first prompt

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print('helo, user<3')
-commandline = str(input('~'))
-print (commandline)
+import pyautogui
+pyautogui.alert('hello, user <3')
+cycle = 1
+while cycle==1:
+    commandline = str(input('~'))  ##ввод комманды
+    if commandline == 'testalert':
+        print(commandline)
+        pyautogui.alert('~ test alert')
+    else:
+        print('~', commandline, ' ', 'is not found')
